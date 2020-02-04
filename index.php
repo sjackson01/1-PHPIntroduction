0c9044027f87cd760a152f02a4b8db9782d57664
Steven Jackson <stevenpjackson0@gmail.com>
2020-02-03 19:38:11 -0600

    Initial Commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Predefined Variables</title>
+</head>
+<body>
+    <?php
+
+        // Create shorthand verions of the variable names 
+        $file = $_SERVER['SCRIPT_FILENAME'];
+        $user = $_SERVER['HTTP_USER_AGENT'];
+        $server = $_SERVER['SERVER_SOFTWARE'];
+
+        // Print the name of this script: 
+        echo "<p>You are running the file: <br /> <strong> $file </strong>. </p> </n>";
+        
+        // Print the users information: 
+        echo "<p>You are viewing this page using: <br /> <strong> $user </strong>. </p> </n>";
+
+        // Print servers information: 
+        echo "<p>This server is running: <br /> <strong> $server </strong>. </p> </n>";
+    ?>
+</body>    
+</html> 
